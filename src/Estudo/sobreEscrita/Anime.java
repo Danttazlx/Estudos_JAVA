2b4a8dc351470fc152397e6101b456d57780d374
daniel <danttaz11@gmail.com>
2025-09-12 21:25:43 -0300

    usando toString para printa

---
+++ src/Estudo/sobreEscrita/Anime.java
@@ -0,0 +1,15 @@
+package Estudo.sobreEscrita;
+
+public class Anime {
+
+    private String nome = "Dantas";
+    private int idade = 19;
+
+
+    @Override
+    public String toString() {
+        return "nome: " + nome + "\n idade: " + idade; //  usando tostring para printa ele vem da classa obj, entao ele
+
+    }
+
+}
---
+++ src/Estudo/sobreEscrita/Test.java
@@ -0,0 +1,9 @@
+package Estudo.sobreEscrita;
+
+public class Test {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+
+        System.out.println(anime);
+    }
+}
