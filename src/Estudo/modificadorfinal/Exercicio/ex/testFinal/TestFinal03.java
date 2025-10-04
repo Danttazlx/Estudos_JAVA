93984b82503475a023fcbeea1d43ea9027e77f72
daniel <danttaz11@gmail.com>
2025-10-04 01:47:38 -0300

    metodo final

--- src/Estudo/modificadorfinal/Exercicio/ex/testFinal/TestFinal03.java
+++ src/Estudo/modificadorfinal/Exercicio/ex/testFinal/TestFinal03.java
@@ -1,4 +1,15 @@
 package Estudo.modificadorfinal.Exercicio.ex.testFinal;
 
+import Estudo.modificadorfinal.Exercicio.ex.dominio.Ferrari;
+
 public class TestFinal03 {
+    public static void main(String[] args) {
+
+        Ferrari ferrari = new Ferrari();
+        ferrari.imprime();       // metodos final nao pode ser instanciado.
+
+
+
+
+    }
 }
